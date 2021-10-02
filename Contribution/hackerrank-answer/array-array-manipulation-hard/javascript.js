1490732ba33422abc40c9ca1f1bbb7b8bf73d8a5
Abui <adjiem31@gmail.com>
2021-10-02 20:28:22 +0700

    add: answer for array manipulation

---
+++ Contribution/hackerrank-answer/array-array-manipulation-hard/javascript.js
@@ -0,0 +1,73 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'arrayManipulation' function below.
+ *
+ * The function is expected to return a LONG_INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. 2D_INTEGER_ARRAY queries
+ */
+
+function arrayManipulation(n, queries) {
+    // Write your code here
+     let arr = new Array(n + 1).fill(0);
+
+    queries.forEach(([a, b, k]) => {
+            arr[a-1] += k;
+            arr[b] -=k;
+        
+    })
+    let sum = 0
+    let max = 0;
+    arr.forEach(val => {
+        sum += val;
+        max = Math.max(sum, max)
+    })
+    return max
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const firstMultipleInput = readLine().replace(/\s+$/g, '').split(' ');
+
+    const n = parseInt(firstMultipleInput[0], 10);
+
+    const m = parseInt(firstMultipleInput[1], 10);
+
+    let queries = Array(m);
+
+    for (let i = 0; i < m; i++) {
+        queries[i] = readLine().replace(/\s+$/g, '').split(' ').map(queriesTemp => parseInt(queriesTemp, 10));
+    }
+
+    const result = arrayManipulation(n, queries);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
---
+++ Contribution/hackerrank-answer/array-array-manipulation-hard/readme.md
@@ -0,0 +1,3 @@
+## Interview Preparation Kit /  Array / Array Manipulation Answer
+
+This is answer for finishing [Level Hard - Array Manipulation](https://www.hackerrank.com/challenges/crush/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=arrays) of hackerrank, which is very hard.
