5cebe351db8c43c545eeda9b3e38aa33ac53ef1e
RaviAnand111 <yravianand111@gmail.com>
2021-10-03 15:20:13 -0400

    Added LinearSearch and BinarySearch algorightms in c and added Searching folder in C folder

--- C/Searching-Algorithms/BinarySearch.c
+++ C/Searching-Algorithms/BinarySearch.c
@@ -1,6 +1,5 @@
-// Binary Search in C++
-#include <bits/stdc++.h>
-using namespace std;
+// Binary Search in C
+#include <stdio.h>
 
 // A recursive binary search function. It returns
 // location of x in given array arr[l..r] is present,
@@ -33,11 +32,12 @@ int binarySearch(int arr[], int l, int r, int x)
 int main(void)
 {
 	int arr[] = { 2, 3, 4, 10, 40 };
-	int x = 10;
 	int n = sizeof(arr) / sizeof(arr[0]);
+	int x = 10;
 	int result = binarySearch(arr, 0, n - 1, x);
-	(result == -1) ? cout << "Element is not present in array"
-				: cout << "Element is present at index " << result;
+	(result == -1) ? printf("Element is not present in array")
+				: printf("Element is present at index %d",
+							result);
 	return 0;
 }
 
